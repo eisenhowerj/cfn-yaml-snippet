ac3cb046d44671ec0490a391860b4502d91eb41d
John E <eisenhowerj@gmail.com>
2019-04-04 22:18:45 -0400

    azure pipeline vars not working, lame hackaround

--- check-for-update.py
+++ check-for-update.py
@@ -5,7 +5,9 @@ import requests
 import sys
 
 # md5 of last update stored in azure pipeline variables
-old_hash=sys.argv[1]
+# old_hash=sys.argv[1]
+# file to store hash in
+hash_file="current-json-spec-hash"
 # Specs from us-east-1 https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cfn-resource-specification.html
 url="https://d1uauaxba7bl26.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json"
 
@@ -13,10 +15,17 @@ url="https://d1uauaxba7bl26.cloudfront.net/latest/gzip/CloudFormationResourceSpe
 response = requests.get(url)
 new_hash = hashlib.md5(response.content).hexdigest()
 
-# If we match previous run, die (killing the pipeline)
+# Load the current hash
+with open(hash_file, 'r') as text_file:
+    old_hash = text_file.read()
+print(old_hash)
+
+# If we match previous run, die (blocking the pipeline)
 if new_hash == old_hash:
+    print("Hash matches, die")
     exit(1)
 
 # echo out new hash so pipeline script can update azure variable
-print(new_hash)
-print("##vso[task.setvariable variable=compare.md5]",new_hash)
+
+with open(hash_file, "w") as text_file:
+    text_file.write(new_hash)
