b7e6ba778eb373fe28d4050559d07ebb0f40c136
John E <eisenhowerj@gmail.com>
2019-04-04 22:19:07 -0400

    and the script updates

--- check-for-update.py
+++ check-for-update.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
+######################
+# Azure pipeline runs on a schedule
+# We get md5 hash of Amazon's spec file and compare to previous hash stored in file
+# If it matches, exit 1 thus blocking the pipeline from continuing
+# If it doesn't, update the file and git the file so next run will/won't be blocked
 
 import hashlib
 import requests
 import sys
 
-# md5 of last update stored in azure pipeline variables
-# old_hash=sys.argv[1]
-# file to store hash in
+# md5 of last update stored in a file
 hash_file="current-json-spec-hash"
 # Specs from us-east-1 https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cfn-resource-specification.html
 url="https://d1uauaxba7bl26.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json"
@@ -18,7 +21,6 @@ new_hash = hashlib.md5(response.content).hexdigest()
 # Load the current hash
 with open(hash_file, 'r') as text_file:
     old_hash = text_file.read()
-print(old_hash)
 
 # If we match previous run, die (blocking the pipeline)
 if new_hash == old_hash:
@@ -26,6 +28,5 @@ if new_hash == old_hash:
     exit(1)
 
 # echo out new hash so pipeline script can update azure variable
-
 with open(hash_file, "w") as text_file:
     text_file.write(new_hash)
